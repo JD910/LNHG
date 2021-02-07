90135944e0d0684acae12a1c8e87cd861b052e08
JD910 <42693677+JD910@users.noreply.github.com>
2021-02-07 09:34:06 +0800

    Update Train_Disc.py

--- Train_Disc.py
+++ Train_Disc.py
@@ -5,30 +5,28 @@ from torch.autograd import Variable
 
 
 def Train_Disc(self, free_img, noised_img, train=True):
-
+    
+    for p in self.generator.parameters():
+        p.requires_grad = False  # to avoid computation
+    for p in self.discriminator.parameters():
+        p.requires_grad = True  # to active computation
 
     self.D_optimizer.zero_grad() 
     z = Variable(noised_img)
-    #real_img = Variable(free_img / 4096)  
     real_img = Variable(free_img)  
     if self.gpu:
         z = z.cuda()
         real_img = real_img.cuda()
-    #fake_img = self.generator(z,real_img,Disc=True)
+        
     fake_img = self.generator(z)
-
     real_validity = self.discriminator(real_img)
-    #fake_validity = self.discriminator(fake_img.data / 4096) 
     fake_validity = self.discriminator(fake_img.data) 
     gradient_penalty = calc_gradient_penalty(self, real_img.data, fake_img.data)
-
-    d_loss = self.lambda_d_real * torch.mean(-real_validity) + self.lambda_d_fake * torch.mean(fake_validity) + self.lambda_gp * gradient_penalty
+    d_loss = torch.mean(-real_validity) + torch.mean(fake_validity) + gradient_penalty
     if train:
         d_loss.backward()
-        
         self.D_optimizer.step()
-
-    return d_loss.data.item(), self.lambda_d_real * torch.mean(-real_validity).cpu().item(), self.lambda_d_fake * torch.mean(fake_validity).cpu().item(), self.lambda_gp * gradient_penalty.cpu().item()
+    return d_loss.data.item()
 
 def calc_gradient_penalty(self, free_img, gen_img):
     batch_size = free_img.size()[0]
@@ -47,11 +45,7 @@ def calc_gradient_penalty(self, free_img, gen_img):
         fake = fake.cuda()
     gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
                                 grad_outputs=fake, create_graph=True, retain_graph=True, only_inputs=True)[0]
-
-
-
     gradients = gradients.view(gradients.size(0), -1)
-
     gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
     
     return gradient_penalty
