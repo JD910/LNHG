5056088319d04ea791762cdc7ce8219be7501c9c
JD910 <42693677+JD910@users.noreply.github.com>
2021-03-19 12:38:54 +0800

    Update get_dr_txt.py

--- Detection/get_dr_txt.py
+++ Detection/get_dr_txt.py
@@ -1,8 +1,3 @@
-#----------------------------------------------------#
-#   获取测试集的detection-result和images-optional
-#   具体视频教程可查看
-#   https://www.bilibili.com/video/BV1zE411u7Vw
-#----------------------------------------------------#
 import copy
 import os
 
@@ -20,9 +15,7 @@ from utils.utils import DecodeBox, loc2bbox, nms, get_new_img_size
 
 
 class mAP_FRCNN(FRCNN):
-    #---------------------------------------------------#
-    #   检测图片
-    #---------------------------------------------------#
+
     def detect_image(self,image_id,image):
         self.confidence = 0.01
         self.iou        = 0.45
@@ -31,16 +24,10 @@ class mAP_FRCNN(FRCNN):
         image_shape = np.array(np.shape(image)[0:2])
         old_width, old_height = image_shape[1], image_shape[0]
         old_image = copy.deepcopy(image)
-        
-        #---------------------------------------------------------#
-        #   给原图像进行resize，resize到短边为600的大小上
-        #---------------------------------------------------------#
+
         width,height = get_new_img_size(old_width, old_height)
         image = image.resize([width,height], Image.BICUBIC)
 
-        #-----------------------------------------------------------#
-        #   图片预处理，归一化。
-        #-----------------------------------------------------------#
         photo = np.transpose(np.array(image,dtype = np.float32)/255, (2, 0, 1))
 
         with torch.no_grad():
@@ -50,13 +37,8 @@ class mAP_FRCNN(FRCNN):
 
             roi_cls_locs, roi_scores, rois, _ = self.model(images)
 
-            #-------------------------------------------------------------#
-            #   利用classifier的预测结果对建议框进行解码，获得预测框
-            #-------------------------------------------------------------#
             outputs = self.decodebox.forward(roi_cls_locs[0], roi_scores[0], rois, height = height, width = width, nms_iou = self.iou, score_thresh = self.confidence)
-            #---------------------------------------------------------#
-            #   如果没有检测出物体，返回原图
-            #---------------------------------------------------------#
+
             if len(outputs)==0:
                 return old_image
             outputs = np.array(outputs)
