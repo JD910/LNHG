2bc719adde2679e962c602fd107f58328e052c3f
JD910 <42693677+JD910@users.noreply.github.com>
2021-03-19 10:25:03 +0800

    Create train.py

---
+++ Segmentation/Detection/train.py
@@ -0,0 +1,184 @@
+import time
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from nets.frcnn import FasterRCNN
+from trainer import FasterRCNNTrainer
+from utils.dataloader import FRCNNDataset, frcnn_dataset_collate
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+        
+def fit_ont_epoch(net,epoch,epoch_size,epoch_size_val,gen,genval,Epoch,cuda):
+    total_loss = 0
+    rpn_loc_loss = 0
+    rpn_cls_loss = 0
+    roi_loc_loss = 0
+    roi_cls_loss = 0
+    val_toal_loss = 0
+    with tqdm(total=epoch_size,desc=f'Epoch {epoch + 1}/{Epoch}',mininterval=0.3) as pbar:
+        for iteration, batch in enumerate(gen):
+            if iteration >= epoch_size:
+                break
+            imgs, boxes, labels = batch[0], batch[1], batch[2]
+
+            with torch.no_grad():
+                if cuda:
+                    imgs = Variable(torch.from_numpy(imgs).type(torch.FloatTensor)).cuda()
+                else:
+                    imgs = Variable(torch.from_numpy(imgs).type(torch.FloatTensor))
+
+            losses = train_util.train_step(imgs, boxes, labels, 1)
+            rpn_loc, rpn_cls, roi_loc, roi_cls, total = losses
+            total_loss += total.item()
+            rpn_loc_loss += rpn_loc.item()
+            rpn_cls_loss += rpn_cls.item()
+            roi_loc_loss += roi_loc.item()
+            roi_cls_loss += roi_cls.item()
+            
+            pbar.set_postfix(**{'total'    : total_loss / (iteration + 1), 
+                                'rpn_loc'  : rpn_loc_loss / (iteration + 1),  
+                                'rpn_cls'  : rpn_cls_loss / (iteration + 1), 
+                                'roi_loc'  : roi_loc_loss / (iteration + 1), 
+                                'roi_cls'  : roi_cls_loss / (iteration + 1), 
+                                'lr'       : get_lr(optimizer)})
+            pbar.update(1)
+
+    print('Start Validation')
+    with tqdm(total=epoch_size_val, desc=f'Epoch {epoch + 1}/{Epoch}',mininterval=0.3) as pbar:
+        for iteration, batch in enumerate(genval):
+            if iteration >= epoch_size_val:
+                break
+            imgs,boxes,labels = batch[0], batch[1], batch[2]
+
+            with torch.no_grad():
+                if cuda:
+                    imgs = Variable(torch.from_numpy(imgs).type(torch.FloatTensor)).cuda()
+                else:
+                    imgs = Variable(torch.from_numpy(imgs).type(torch.FloatTensor))
+
+                train_util.optimizer.zero_grad()
+                losses = train_util.forward(imgs, boxes, labels, 1)
+                _, _, _, _, val_total = losses
+
+                val_toal_loss += val_total.item()
+
+            pbar.set_postfix(**{'total_loss': val_toal_loss / (iteration + 1)})
+            pbar.update(1)
+
+    print('Finish Validation')
+    print('Epoch:'+ str(epoch+1) + '/' + str(Epoch))
+    print('Total Loss: %.4f || Val Loss: %.4f ' % (total_loss/(epoch_size+1),val_toal_loss/(epoch_size_val+1)))
+    print('Saving state, iter:', str(epoch+1))
+    torch.save(model.state_dict(), 'logs/Epoch%d-Total_Loss%.4f-Val_Loss%.4f.pth'%((epoch+1),total_loss/(epoch_size+1),val_toal_loss/(epoch_size_val+1)))
+    
+if __name__ == "__main__":
+
+    Cuda = True
+    
+    NUM_CLASSES = 2 #Nodule = 1
+
+    input_shape = [512,512,3]
+
+    backbone = "resnet50"
+    model = FasterRCNN(NUM_CLASSES,backbone = backbone)
+
+    #model_path = 'model_data/voc_weights_resnet.pth'
+    model_path = './Pretrained_Model.pth'
+    print('Loading weights into state dict...')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model_dict = model.state_dict()
+    pretrained_dict = torch.load(model_path, map_location=device)
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if np.shape(model_dict[k]) ==  np.shape(v)}
+    model_dict.update(pretrained_dict)
+    model.load_state_dict(model_dict)
+    print('Finished!')
+
+    net = model.train()
+    if Cuda:
+        net = torch.nn.DataParallel(model)
+        cudnn.benchmark = True
+        net = net.cuda()
+
+    annotation_train_path = './Train.txt'
+
+    with open(annotation_train_path) as f:
+        lines_train = f.readlines()
+    np.random.seed(10101)
+    np.random.shuffle(lines_train)
+    np.random.seed(None)
+    num_train = len(lines_train)
+
+    annotation_test_path = './Tval.txt'
+
+    with open(annotation_test_path) as f:
+        lines_test = f.readlines()
+    np.random.seed(10101)
+    np.random.shuffle(lines_test)
+    np.random.seed(None)
+    num_val = len(lines_test)
+
+    if False:
+        lr = 1e-4
+        Batch_size = 2
+        Init_Epoch = 0
+        Freeze_Epoch = 50
+        
+        optimizer = optim.Adam(net.parameters(), lr, weight_decay=5e-4)
+        lr_scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=1,gamma=0.95)
+
+        train_dataset = FRCNNDataset(lines_train[:num_train], (input_shape[0], input_shape[1]), is_train=True)
+        val_dataset   = FRCNNDataset(lines_test[:num_val], (input_shape[0], input_shape[1]), is_train=False)
+        gen     = DataLoader(train_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                                drop_last=True, collate_fn=frcnn_dataset_collate)
+        gen_val = DataLoader(val_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                                drop_last=True, collate_fn=frcnn_dataset_collate)
+                        
+        epoch_size = num_train // Batch_size
+        epoch_size_val = num_val // Batch_size
+
+        for param in model.extractor.parameters():
+            param.requires_grad = False
+
+        model.freeze_bn()
+
+        train_util = FasterRCNNTrainer(model, optimizer)
+
+        for epoch in range(Init_Epoch,Freeze_Epoch):
+            fit_ont_epoch(net,epoch,epoch_size,epoch_size_val,gen,gen_val,Freeze_Epoch,Cuda)
+            lr_scheduler.step()
+
+    if True:
+        lr = 1e-5
+        Batch_size = 10
+        Freeze_Epoch = 50
+        Unfreeze_Epoch = 100
+
+        optimizer = optim.Adam(net.parameters(), lr, weight_decay=5e-4)
+        lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
+
+        train_dataset = FRCNNDataset(lines_train[:num_train], (input_shape[0], input_shape[1]), is_train=True)
+        val_dataset   = FRCNNDataset(lines_test[:num_val], (input_shape[0], input_shape[1]), is_train=False)
+        gen     = DataLoader(train_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                                drop_last=True, collate_fn=frcnn_dataset_collate)
+        gen_val = DataLoader(val_dataset, shuffle=True, batch_size=Batch_size, num_workers=4, pin_memory=True,
+                                drop_last=True, collate_fn=frcnn_dataset_collate)
+                        
+        epoch_size = num_train // Batch_size
+        epoch_size_val = num_val // Batch_size
+
+        for param in model.extractor.parameters():
+            param.requires_grad = True
+
+        model.freeze_bn()
+
+        train_util = FasterRCNNTrainer(model,optimizer)
+
+        for epoch in range(Freeze_Epoch,Unfreeze_Epoch):
+            fit_ont_epoch(net,epoch,epoch_size,epoch_size_val,gen,gen_val,Unfreeze_Epoch,Cuda)
+            lr_scheduler.step()
